9eab04f2270a0e263687a1e5b295b76f902f3c34
Jordon Bedwell <envygeeks@gmail.com>
2013-07-03 01:37:45 -0500

    Add specs for core extensions.

---
+++ spec/lib/content/pipeline/core_extensions/object_ext_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec/helper'
+
+describe CoreExtensions::ObjectExt do
+  subject do
+    described_class
+  end
+
+  describe '#to_nokogiri_fragment' do
+    it 'converts an object to a nokogiri fragment' do
+      expect("".to_nokogiri_fragment).to be_kind_of Nokogiri::HTML::DocumentFragment
+    end
+  end
+end
