18955e309096eb7c14cf1d87a96a7099cd3fa867
Jordon Bedwell <envygeeks@gmail.com>
2013-07-03 03:27:23 -0500

    Add require or fail to Object.

--- lib/content/pipeline/core_extensions/object_ext.rb
+++ lib/content/pipeline/core_extensions/object_ext.rb
@@ -16,6 +16,21 @@ module CoreExtensions
       return self if Nokogiri::HTML::DocumentFragment === self
       Nokogiri::HTML.fragment(respond_to?(:to_html) ? to_html : to_s)
     end
+
+    # ------------------------------------------------------------------------
+    # Require a file with a fallback and if all else fails send an error.
+    # ------------------------------------------------------------------------
+
+    def require_or_fail(a, b, msg)
+      require a
+    rescue LoadError
+      begin
+        require b
+      rescue LoadError => error
+        # Try to keep the call chain proper.
+        raise LoadError, msg, error.backtrace[3...-1]
+      end
+    end
   end
 end
 
