bef8a3c5b590d0c9107c8309a2ed5e9983ca8825
Jordon Bedwell <envygeeks@gmail.com>
2014-02-08 23:36:28 -0600

    Do some cleanup.

--- lib/content/pipeline.rb
+++ lib/content/pipeline.rb
@@ -7,27 +7,45 @@ module Content
     attr_reader :filters, :opts
 
     # -----------------------------------------------------------------
+    # @arg Array: The filters you would like to use or next:
+    # @arg Hash : Your "default options" that should be "global."
+    # -----------------------------------------------------------------
 
-    def initialize(filters = Filters::DEFAULT_FILTERS, opts = {})
+    def initialize(filters = nil, opts = nil)
+      if filters.is_a?(Hash) && opts.nil?
+        opts = filters
+        filters =  nil
+      end
+
+
+      opts = {} if opts.nil?
+      filters = Filters::DEFAULT_FILTERS if filters.nil?
       @opts, @filters = opts.freeze, [ filters ].flatten.freeze
     end
 
     # -----------------------------------------------------------------
+    # @arg String: The incomming string that will be modified.
+    # @arg Hash  : The secondary opts to override the "defaults".
+    # -----------------------------------------------------------------
 
     def filter(out, opts = {})
       opts = @opts.deep_merge(opts)
-      @filters.each do |f|
-        out_opts = opts.values_at(*to_opt(f)).delete_if(&:nil?)
-        out = f.new(out, out_opts.reduce(Hash.new, :merge)).run
+      @filters.each_with_index do |f, i|
+        fopts = opts.values_at(*to_opt(f)). \
+          delete_if(&:nil?).reduce(Hash.new, :merge)
+
+        out = f.new(out, fopts).run(@filters[i + 1])
       end
 
     out
     end
 
     # -----------------------------------------------------------------
+    # @arg Object: An object (preferably a class or module.)
+    # -----------------------------------------------------------------
 
     private
-    def to_opt(cls, alt = false)
+    def to_opt(cls)
       cls = cls.name.split(/::/).last
       out = [
         cls.downcase,
