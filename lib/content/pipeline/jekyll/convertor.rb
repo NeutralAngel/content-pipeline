c6da27f854fc0dd0573a3cc38b3e730bbabe5009
Jordon Bedwell <envygeeks@gmail.com>
2014-06-03 04:31:39 -0500

    Add a Jekyll convertor into ContentPipeline.

---
+++ lib/content/pipeline/jekyll/convertor.rb
@@ -0,0 +1,27 @@
+require "content/pipeline"
+
+module Content::Pipeline::Jekyll
+  class Convertor < Jekyll::Convertor
+    CONTENT_PIPELINE = Content::Pipeline.new
+
+    def matches(ext)
+      ext =~ /\.(md|markdown)\Z/
+    end
+
+    # -------------------------------------------------------------------------
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    # -------------------------------------------------------------------------
+    # It should be noted that MARKDOWN_CONVERTOR uses the default options
+    # provided by Content::Pipeline at:
+    #   https://github.com/envygeeks/content-pipeline
+    # -------------------------------------------------------------------------
+
+    def convert(data)
+      CONTENT_PIPELINE.filter(data)
+    end
+  end
+end
