195a37f2ca85d8d63cc2d769f1dd97de7cc5d274
Jordon Bedwell <jordon@envygeeks.io>
2014-08-29 01:20:46 -0500

    Just a small rename because I can.

--- lib/content/pipeline/jekyll/converter.rb
+++ lib/content/pipeline/jekyll/converter.rb
@@ -2,26 +2,18 @@ require "content/pipeline"
 
 module Content::Pipeline::Jekyll
   class Converter < Jekyll::Converter
-    CONTENT_PIPELINE = Content::Pipeline.new
+    ContentPipeline = Content::Pipeline.new
 
     def matches(ext)
       ext =~ /\.(md|markdown)\Z/
     end
 
-    # -------------------------------------------------------------------------
-
     def output_ext(ext)
       ".html"
     end
 
-    # -------------------------------------------------------------------------
-    # It should be noted that MARKDOWN_CONVERTOR uses the default options
-    # provided by Content::Pipeline at:
-    #   https://github.com/envygeeks/content-pipeline
-    # -------------------------------------------------------------------------
-
     def convert(data)
-      CONTENT_PIPELINE.filter(data)
+      ContentPipeline.filter(data)
     end
   end
 end
